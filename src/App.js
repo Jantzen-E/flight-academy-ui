108b25694472d344694e20161676a740bddbc239
Jantzen-E <jantzen.egan@gmail.com>
2020-06-11 00:06:29 -0600

    added landing page for scheduling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './components/signUpForm';
+import signUpForm from './components/signUpForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sign up for classes here</h1>
+      <h2>Click the time that you want to schedule</h2>
+      <div className="daysAndClasses">
+        <div>
+          <h3>Monday</h3>
+          <h4 className="canSchedule">10 - 11 Tumbling</h4>
+          <h4 className="canSchedule">11 - 12 Cheer</h4>
+        </div>
+        <br/>
+        <div>
+          <h3>Tuesday</h3>
+          <h4 className="cannotSchedule">No classes</h4>
+        </div>
+        <br/>
+        <div>
+          <h3>Wednesday</h3>
+          <h4 className="canSchedule">9 - 10 Tumbling</h4>
+          <h4 className="canSchedule">10 - 11 Cheer</h4>
+        </div>
+        <br/>
+        <div>
+          <h3>Thursday</h3>
+          <h4 className="cannotSchedule">No classes</h4>
+        </div>
+        <br/>
+        <div>
+          <h3>Friday</h3>
+          <h4 className="canSchedule">10 - 11 Tumbling</h4>
+          <h4 className="canSchedule">11 - 12 Cheer</h4>
+        </div>
+        <br/>
+        <div>
+          <h3>Saturday</h3>
+          <h4 className="canSchedule">10 - 11 Tumbling</h4>
+          <h4 className="canSchedule">11 - 12 Cheer</h4>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/signUpForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class signUpForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Hello from sign up form page</h1>
+            </div>
+        );
+    }
+}
+
+export default signUpForm;
